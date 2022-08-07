8e480b8888780853ffb4a29e7ea02b48503fd48e
sourav <sauravkumarji99@gmail.com>
2022-08-07 21:21:56 +0530

    In this commit I learn abpoout component how component work in App.js How we can import as well

---
+++ blog/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import User from './User'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sourav</h1>
+      <User/>
+    </div>
+    
+  );
+}
+
+
+export default App;
---
+++ blog/src/User.js
@@ -0,0 +1,11 @@
+function User ()
+{
+
+    return(
+        <div>
+        <h1>This is me Sourav</h1>
+        </div>
+    )      
+}
+
+export default User;
