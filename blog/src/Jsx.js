6d0fe10e8e20afde827985ff373c0649809fac9c
sourav <sauravkumarji99@gmail.com>
2022-08-17 23:00:34 +0530

    In this commit you will learn That how can we Use HTML without JSX

--- blog/src/Jsx.js
+++ blog/src/Jsx.js
@@ -1,8 +1,8 @@
+import React from "react";
 function  Jsx ()
 {
-  return(
-    <h1>This is Jsx</h1>
-  )
+  return React.createElement('div', null, React.createElement('h1', null,"I am using inner Tag " )) //This is how complexity you will have to face when you will use Html withou jsx
+    // Without Jsx you can use html , but this is very tough task for using yoiu have to first import the React
 
 }
 
