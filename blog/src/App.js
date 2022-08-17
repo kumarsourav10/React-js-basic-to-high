091abac0f606b89c49332d4979f2b6d2d53aca1b
sourav <sauravkumarji99@gmail.com>
2022-08-17 22:19:55 +0530

    I added class component and return function inside a function

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import User from './User'
 import Sourav from './Sourav'
 import Form from './Form';
-
+import User1 from './User1';
 function App() {
+
+  function Apple() 
+  {
+    return (<div>Apple Count</div>)
+  }
   return (
     <div className="App">
       <h1>Hello Sourav</h1>
       <User />
       <Sourav />
       <Form />
+      <User1 />
+      {Apple ()}
     </div>
   );
 }
