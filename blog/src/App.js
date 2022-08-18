a558fd2fb6fa6cd26409563b506ecfed00f562c5
sourav <sauravkumarji99@gmail.com>
2022-08-18 11:04:37 +0530

    added click event and learn alert function

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,27 +1,40 @@
 // import logo from './logo.svg';
 import './App.css';
-import User from './User'
-import Sourav from './Sourav'
-import Form from './Form';
-import User1 from './User1';
-import Check from './Check';
-import Jsx from './Jsx';
+// import User from './User'
+// import Sourav from './Sourav'
+// import Form from './Form';
+// import User1 from './User1';
+// import Check from './Check';
+// import Jsx from './Jsx';
+// alert("Hello")
 function App() {
-
-  function Apple() 
+    let data = "Sourav Pandit"
+  function apple() 
   {
-    return (<div>Apple Count</div>)
+    data = "peter"
+   alert(data)
   }
+
+  // function Apple() 
+  // {
+  //   return (<div>Apple Count</div>)
+  // }
   return (
     <div className="App">
-      <h1>Hello Sourav</h1>
-      <User />
+      <h1>{data}</h1>
+      {/* <User />
       <Sourav />
       <Form />
       <User1 />
       <Apple />
       <Check />
-      <Jsx />
+      <Jsx /> */}
+
+      {/* Whenever you give onClick event and give the alert it will come first without click it will appear first . How you prevent this after using  onClick's function ypu have to just remove the brackets which is return just after the apple `apple()`   */}
+      {/* <button onClick={apple()}>Click Me</button> */}
+
+      {/* See here how I removed the brackets */}
+      <button onClick={()=>apple("Hello world")}>Click Me</button>
     </div>
   );
 }
--- blog/src/Jsx.js
+++ blog/src/Jsx.js
@@ -1,10 +1,10 @@
-import React from "react";
-function  Jsx ()
-{
-  return React.createElement('div', null, React.createElement('h1', null,"I am using inner Tag " )) //This is how complexity you will have to face when you will use Html withou jsx
-    // Without Jsx you can use html , but this is very tough task for using yoiu have to first import the React
+// import React from "react";
+// function  Jsx ()
+// {
+//   return React.createElement('div', null, React.createElement('h1', null,"I am using inner Tag " )) //This is how complexity you will have to face when you will use Html withou jsx
+//     // Without Jsx you can use html , but this is very tough task for using yoiu have to first import the React
 
-}
+// }
 
 
-export default Jsx;
+// export default Jsx;
