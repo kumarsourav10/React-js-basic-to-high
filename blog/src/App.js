f25d415869c6c6ec8fc3d594933b21366e81d8cd
sourav <sauravkumarji99@gmail.com>
2022-08-18 12:27:28 +0530

    so that's how you can use state to store data in your state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,40 +1,18 @@
+import {useState} from 'react'
 // import logo from './logo.svg';
 import './App.css';
-// import User from './User'
-// import Sourav from './Sourav'
-// import Form from './Form';
-// import User1 from './User1';
-// import Check from './Check';
-// import Jsx from './Jsx';
-// alert("Hello")
-function App() {
-    let data = "Sourav Pandit"
-  function apple() 
-  {
-    data = "peter"
-   alert(data)
-  }
 
-  // function Apple() 
-  // {
-  //   return (<div>Apple Count</div>)
-  // }
+function App() {
+const [data,setData]=useState("Sourav")
+function updateData()
+{
+  setData("Pandit")
+}
+console.warn("_________")
   return (
     <div className="App">
       <h1>{data}</h1>
-      {/* <User />
-      <Sourav />
-      <Form />
-      <User1 />
-      <Apple />
-      <Check />
-      <Jsx /> */}
-
-      {/* Whenever you give onClick event and give the alert it will come first without click it will appear first . How you prevent this after using  onClick's function ypu have to just remove the brackets which is return just after the apple `apple()`   */}
-      {/* <button onClick={apple()}>Click Me</button> */}
-
-      {/* See here how I removed the brackets */}
-      <button onClick={()=>apple("Hello world")}>Click Me</button>
+      <button onClick={updateData} >Update Data</button>
     </div>
   );
 }
