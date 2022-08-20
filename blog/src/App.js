0319f7b13832fbcdca172db9ab094f47a8c36e22
sourav <sauravkumarji99@gmail.com>
2022-08-20 13:39:39 +0530

    this is how you can define prop one by one

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,10 +5,10 @@ import Student from './Student';
 function App() {
   return(
     <div className="App">
-      <h1>Student Data</h1>
-      <Student  allinone={{name:"Sumit", address: "Noida", Mobile: 8368149399, Height: "5'6", Weight: "90"}}/>
-      <Student allinone={{name:"Amit", address: "Delhi", Mobile: 8799709424, Height: "4'6", Weight: "80"}}/>
-      <Student allinone={{name:"Akshay", address: "GuruGram", Mobile: 7827901829, Height: "5'11", Weight: "75"}}/>
+      <h1>Props in React :</h1>
+      <Student name={"Sakshi"} email="Sakshi@test.com"  address="Noida"/>
+      <Student name={"Sourav"} email="Sourav@test.com" address="Delhi"/>
+      <Student name={"Akshay"} email="Akshay@test.com"  address="up"/>
     </div>
   )
 }
--- blog/src/Student.js
+++ blog/src/Student.js
@@ -4,11 +4,9 @@ function Student(props)
     console.log(props.name)
     return(
     <div style={{backgroundColor: "skyblue"}}>
-        <h1>Name :{props.allinone.name}</h1>
-        <h2>Address : {props.allinone.address}</h2>
-        <h3>Mobile : {props.allinone.Mobile}</h3>
-        <h4>Height : {props.allinone.Height}</h4>
-        <h4>Weight : {props.allinone.Weight}</h4>
+        <h1>Name :{props.name}</h1>
+        <h2>Address : {props.address}</h2>
+        <h3>Email : {props.email}</h3>
     </div>
     )
 } 
