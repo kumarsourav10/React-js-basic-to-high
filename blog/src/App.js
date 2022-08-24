e6c55fb8b5d96639fb678392ab2906a67a416e06
sourav <sauravkumarji99@gmail.com>
2022-08-24 17:20:54 +0530

    learn get input in function

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,6 +2,7 @@
 import './App.css'
 import Butun from './Class'
 import React from 'react'
+import GetInpu from './GetInpu';
 
 class App extends React.Component {
      constructor()
@@ -19,11 +20,12 @@ class App extends React.Component {
           <div className='App'>
       <h1>Props</h1>
       <Butun name={this.state.name} email="Priyanshu@gmail.com"></Butun>
+      <GetInpu />
       <button onClick={()=>this.setState({name:"Beena Devi"})}>Update Name</button>
     </div>
            );
+          }
         }
-      }
-      
+        
 
 export default App
